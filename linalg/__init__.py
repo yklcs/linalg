1a280f4d7106865a526f89b54464c53fa9c3c71e
RocketLL <luc4sl33@gmail.com>
2019-06-24 08:05:13 +0900

    edit for sphinx

--- linalg/__init__.py
+++ linalg/__init__.py
@@ -1,3 +1,7 @@
+"""Main functions and classes"""
+
+__all__ = ["Matrix", "zeroes", "identity", "random_matrix", "solve", "lu"]
+
 from linalg.matrix import Matrix
 from linalg.create import *
 from linalg.decompose import *

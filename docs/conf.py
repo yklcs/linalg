f9e8f5ddce64006f6304ecd2d1254f04624da15c
RocketLL <luc4sl33@gmail.com>
2019-06-25 01:35:41 +0900

    bump to 1.0.2

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ copyright = "2019, Lucas Lee"
 author = "Lucas Lee"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.1"
+release = "1.0.2"
 
 
 # -- General configuration ---------------------------------------------------
